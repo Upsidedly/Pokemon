00bb4831f60354397301d1cd67f3f13160c140ea
Upsided <matthewlloydw@gmail.com>
2021-11-25 16:44:02 -0500

    added

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ version = 1.0
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
+WIDTH = 1280
+HEIGHT = 720
+dim = (WIDTH, HEIGHT)
+
+screen = pygame.display.set_mode(dim, pygame.RESIZABLE)
 pygame.display.set_caption(f'Pokemon First Form Edition v.{version}')
 
 run = True
+clock = pygame.time.Clock()
+
 while run:
     for event in pygame.event.get():
         if event == pygame.QUIT:
             pygame.quit()
+            sys.exit()
             run = False
 
     pygame.display.update()
-    pygame.time.Clock.tick(120)
+    clock.tick(120)
+
