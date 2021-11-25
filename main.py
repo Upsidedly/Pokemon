90997f3080bed1fb73348ae168b5c447fe0d7cc5
Upsided <matthewlloydw@gmail.com>
2021-11-25 16:37:36 -0500

    added

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-# This is a sample Python script.
-print('hi')
+import pygame, sys, pickle, os
+
+version = 1.0
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption(f'Pokemon First Form Edition v.{version}')
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            pygame.quit()
+            run = False
+
+    pygame.display.update()
+    pygame.time.Clock.tick(120)
